bab7e4523dd4c6d2a5518bda9c9a69ce913085e3
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-03-16 14:43:11 +0100

    Refactor the configuration data.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import requests
+from transformers import *
 
 
 def download_file_from_google_drive(id_token, destination):
@@ -47,9 +48,60 @@ def check_for_component(destination, file_drive_id):
         download_file_from_google_drive(file_drive_id, destination)
 
 
-# pretrained model name: (model class, model tokenizer, output dimension, token style)
-VAD_MODELS = {'base': torch.hub.load(repo_or_dir="../models/basic_vad",
-                                     source='local', model='silero_vad', force_reload=True),
-              'gtm-base': './pykaldi/sad/SAD.sh'}
+TOKEN_IDX = {
+    'bert': {
+        'START_SEQ': 101,
+        'PAD': 0,
+        'END_SEQ': 102,
+        'UNK': 100
+    },
+    'xlm': {
+        'START_SEQ': 0,
+        'PAD': 2,
+        'END_SEQ': 1,
+        'UNK': 3
+    },
+    'roberta': {
+        'START_SEQ': 0,
+        'PAD': 1,
+        'END_SEQ': 2,
+        'UNK': 3
+    },
+    'albert': {
+        'START_SEQ': 2,
+        'PAD': 0,
+        'END_SEQ': 3,
+        'UNK': 1
+    },
+}
 
-SPK_DIARIZATION_MODELS = {'base': None}
+# 'O' -> No punctuation
+punctuation_dict = {'O': 0, 'COMMA': 1, 'PERIOD': 2, 'QUESTION': 3, 'ALL_CAPITAL': 4, 'FRITS_CAPITAL': 5,
+                    'ALL_CAPITAL+COMMA': 6, 'ALL_CAPITAL+PERIOD': 7, 'ALL_CAPITAL+QUESTION': 8,
+                    'FRITS_CAPITAL+COMMA': 9, 'FRITS_CAPITAL+PERIOD': 10, 'FRITS_CAPITAL+QUESTION': 11}
+
+transformation_dict = {0: lambda x: x.lower(), 1: (lambda x: x + ','), 2: (lambda x: x + '.'),
+                       3: (lambda x: x + '?'),
+                       4: lambda x: x.upper(), 5: (lambda x: x[0].upper() + x[1:]), 6: (lambda x: x.upper() + ','),
+                       7: (lambda x: x.upper() + '.'), 8: (lambda x: x.upper() + '?'),
+                       9: (lambda x: x[0].upper() + x[1:] + ','), 10: (lambda x: x[0].upper() + x[1:] + '.'),
+                       11: (lambda x: x[0].upper() + x[1:] + '?')}
+
+
+# pretrained model name: (model class, model tokenizer, output dimension, token style)
+ALR_MODELS = {'base': None,
+              'gtm': None}
+
+VAD_MODELS = {'base': torch.hub.load(repo_or_dir="../models/basic_vad", source='local',
+                                     model='silero_vad', force_reload=True),
+              'gtm': './pykaldi/sad/SAD.sh'}
+
+SPK_DIARIZATION_MODELS = {'base': torch.hub.load('pyannote/pyannote-audio', 'dia'),
+                          'gtm': './pykaldi/drz/procesaFichero.sh'}
+
+ASR_MODELS = {'base-gl': None,
+              'gtm-gl': './pykaldi/asr/asr.sh'}
+
+ACPRS_MODELS = {'base-gl': None,
+                'gtm-gl': ('../models/bertinho/', BertModel, AutoTokenizer, 768, 'bert', 96, 8),
+                'gtm-spn': ('../models/berto/', BertModel, AutoTokenizer, 768, 'bert', 256, 8)}
