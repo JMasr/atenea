e067b9c01e496cf68a74289d7ad5c739d105b83f
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-03-16 14:40:51 +0100

    Adding new models and utilities.

--- src/model.py
+++ src/model.py
@@ -1,12 +1,20 @@
 import uuid
+import shutil
+import pickle
 import warnings
+import itertools
 import subprocess
-from pyannote.core import Timeline, Segment
+import multiprocessing
+
+import torch.nn as nn
+import torch.nn.functional as function
+import torch.utils.data
+
+from tqdm import tqdm
+from pyannote.core import Timeline, Segment, Annotation
 
 from config import *
-
-from data import Audio
-from data import RichTranscription
+from data import Multimedia, Transcription, Dataset, Subtitle
 
 
 def transform_frame_to_milliseconds(frames, sampling_rate):
@@ -84,8 +92,15 @@ def post_processing_scripting_models_results(result_path):
     with open(result_path, 'r') as f:
         result = f.readlines()
 
+    dir_path = "/".join(result_path.split("/")[:-1])
+    try:
+        shutil.rmtree(dir_path)
+    except OSError as e:
+        print("Error: %s : %s" % (dir_path, e.strerror))
+
     speech_segments = [{'start': segment.split()[0], 'end': segment.split()[1]} for segment in result if'sp' in segment]
     speech_segments = check_time_steps_continuity(speech_segments)
+
     return speech_segments
 
 
@@ -223,6 +238,15 @@ class Model(object):
         pass
 
 
+class ALR(Model):
+    def __init__(self, model_id):
+        super().__init__(model_id, 'ALR')
+        self.model = ALR_MODELS[self.model_id]
+
+    def identify_language(self):
+        return 'gl'
+
+
 class VAD(Model):
     def __init__(self, model_id):
         super().__init__(model_id, 'VAD')
@@ -266,13 +290,13 @@ class VAD(Model):
 
         return speeches
 
-    def apply(self, data=None):
+    def apply(self, multimedia=None):
         """
         This method is used for splitting long audios into speech chunks using a VAD or SAD system
 
         Parameters
         ----------
-        data: list with two objects
+        multimedia: list with two objects
               List with the audio as a tensor in index-0 and sample rate in index-1
 
         Returns
@@ -282,15 +306,16 @@ class VAD(Model):
         acoustic_segments:  list of dicts
                             List containing ends and beginnings of non speech chunks in milliseconds.
         """
-        wav_tensor = data.get_wav_tensor()
-        sampling_rate = data.get_sampling_rate()
+        wav_tensor = multimedia.get_wav_tensor()
+        sampling_rate = multimedia.get_sampling_rate()
 
         if self.model_id == 'base':
             speech_segments = self.run_model_base(wav_tensor, sampling_rate)
             speech_segments = transform_frame_to_milliseconds(speech_segments, sampling_rate)
-        elif self.model_id == 'gtm-base':
-            subprocess.call([self.model, data.get_path(), '../data/output', './pykaldi/sad'])
-            results_path = f'../data/output/{data.name[:-4]}/{data.name[:-4]}.sad'
+        elif self.model_id == 'gtm':
+            path_out = multimedia.get_path().replace('.wav', '_output')
+            subprocess.call([self.model, multimedia.get_path(), path_out, './pykaldi/sad'])
+            results_path = f'{path_out}/{multimedia.get_name().replace(".wav", "")}/{multimedia.get_name().replace(".wav", ".sad")}'
             speech_segments = post_processing_scripting_models_results(results_path)
         else:
             raise TypeError("VAD model id isn't valid.")
@@ -302,12 +327,13 @@ class VAD(Model):
 
 
 class Diarization(Model):
-    def __init__(self, model_id):
+    def __init__(self, model_id, thresholds=None):
         super().__init__(model_id, 'Diarization System')
         self.model = SPK_DIARIZATION_MODELS[self.model_id]
+        self.drz_thresholds = None if not thresholds else thresholds.split(';')
 
         self.spk = {}
-        self.data = None
+        self.diarization_annotations = Annotation()
 
     def get_spk(self):
         return self.spk
@@ -321,8 +347,255 @@ class Diarization(Model):
     def set_data(self, data):
         self.data = data
 
-    def apply(self, new_data):
-        pass
+    def run_model_gtm(self, multimedia):
+        if self.drz_thresholds:
+            th1_1st, th2_1st = self.drz_thresholds[0], self.drz_thresholds[1]
+            th1_2st, th2_2st = self.drz_thresholds[2], self.drz_thresholds[3]
+        else:
+            th1_1st, th2_1st, th1_2st, th2_2st = '13.5', '14.5', '13.5', '14.5'
+
+        path_out = multimedia.get_path().replace('.wav', '_drz_output')
+        subprocess.call([self.model, multimedia.get_path(), path_out, th1_1st, th2_1st, th1_2st, th2_2st])
+
+        path_results = f"{path_out}/{multimedia.get_name().replace('.wav', '_drz.rttm')}"
+        with open(path_results, 'r', encoding="ISO-8859-1") as f:
+            spk_annotations = f.readlines()
+
+        for annotation in spk_annotations:
+            annotation = annotation.split()
+            time_init = float(annotation[3])
+            time_ends = time_init + float(annotation[4])
+            spk_id = annotation[7]
+
+            s = Segment(time_init, time_ends)
+            self.diarization_annotations[s] = spk_id
+
+    def run_model_base(self, multimedia):
+        spk_annotation = self.model({'audio': multimedia.get_path()})
+        self.spk = spk_annotation.labels()
+
+        return spk_annotation
+
+    def apply(self, multimedia):
+        if self.model_id == 'base':
+            self.diarization_annotations = self.run_model_base(multimedia)
+        elif self.model_id == 'gtm':
+            self.run_model_gtm(multimedia)
+        else:
+            raise TypeError("Diarization model-id isn't valid.")
+
+        return self.diarization_annotations
+
+
+class ASR(Model):
+    def __init__(self, model_id):
+        super().__init__(model_id, 'ASR System')
+        self.model = ASR_MODELS[self.model_id]
+
+    def run_gtm(self, multimedia):
+        path_out = multimedia.get_path().replace('.wav', '_output')
+        subprocess.call([self.model, multimedia.get_path(), path_out, './pykaldi/asr'])
+        multimedia_ctm = f"{path_out}/{multimedia.get_name().replace('.wav', '.ctm')}"
+        return Transcription(multimedia_ctm)
+
+    def apply(self, multimedia):
+
+        if 'gtm' in self.model_id:
+            transcription = self.run_gtm(multimedia)
+        else:
+            raise TypeError("ASR model-id isn't valid.")
+
+        return transcription
+
+
+class DeepPunctuation(nn.Module):
+    def __init__(self, model_id, freeze_bert=False, lstm_dim=-1):
+        super(DeepPunctuation, self).__init__()
+
+        self.model_id = model_id
+        self.model_path = ACPRS_MODELS[self.model_id][0]
+
+        self.seq_length = ACPRS_MODELS[self.model_id][5]
+        self.batch_sz = ACPRS_MODELS[self.model_id][6]
+        self.output_dim = len(punctuation_dict)
+
+        self.bert_layer = ACPRS_MODELS[self.model_id][1].from_pretrained(self.model_path)
+        bert_dim = ACPRS_MODELS[self.model_id][3]
+
+        # Freeze bert layers
+        if freeze_bert:
+            for p in self.bert_layer.parameters():
+                p.requires_grad = False
+        if lstm_dim == -1:
+            hidden_size = bert_dim
+        else:
+            hidden_size = lstm_dim
+
+        self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=hidden_size, num_layers=1, bidirectional=True)
+        self.linear = nn.Linear(in_features=hidden_size * 2, out_features=len(punctuation_dict))
+
+    def forward(self, x, attn_masks):
+        if len(x.shape) == 1:
+            x = x.view(1, x.shape[0])  # add dummy batch for single sample
+        # (B, N, E) -> (B, N, E)
+        x = self.bert_layer(x, attention_mask=attn_masks)[0]
+        # (B, N, E) -> (N, B, E)
+        x = torch.transpose(x, 0, 1)
+        x, (_, _) = self.lstm(x)
+        # (N, B, E) -> (B, N, E)
+        x = torch.transpose(x, 0, 1)
+        x = self.linear(x)
+        return x
+
+
+class ACPRS(Model):
+    def __init__(self, model_id):
+        super().__init__(model_id, 'Automatic Capitalization & Punctuation Restoration System')
+        self.model_path = ACPRS_MODELS[self.model_id][0]
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = DeepPunctuation(self.model_id)
+        self.model.to(self.device)
+        self.model.load_state_dict(torch.load(self.model_path + 'weights.pt'))
+
+        self.seq_length = ACPRS_MODELS[self.model_id][5]
+        self.batch_sz = ACPRS_MODELS[self.model_id][6]
+
+        with open('../models/dict.pkl', 'rb') as f:
+            self.dict_cap_words = pickle.load(f)
+
+    def get_final_transcription_and_confidence(self, y_conf: list, y_str: list):
+        """
+        Pre-process the sub-words and tokens output of the model to obedient the final list of word and confidence.
+
+        Parameters
+        ----------
+        y_str: list
+            List with all the sub-words and tokens predicted by the model.
+        y_conf: list
+            List with the prediction confidence of the model for each element in y_str.
+
+        Returns
+        ----------
+        new_text: List with all the words of the transcription with capitalization and punctuation marks.
+        new_confidence: List with the prediction confidence of the model for each word.
+        """
+
+        ind, new_text, new_confidence, join_word = 0, [], [], False
+        while ind < len(y_str) - 1:
+            if y_str[ind] in ['£', '¢', '[pad]', '[PAD]']:
+                ind += 1
+                continue
+            elif (ind != 0) and ("#" in y_str[ind]):
+                new_text[-1] = new_text[-1] + y_str[ind][2:]
+                new_confidence[-1] = max(y_conf[ind], y_conf[ind - 1])
+                ind += 1
+                continue
+            elif (ind != len(y_str) - 1) and ("#" in y_str[ind + 1]):
+                new_t = y_str[ind] + y_str[ind + 1][2:]
+                new_c = max(y_conf[ind], y_conf[ind + 1])
+                ind += 2
+            else:
+                new_t = y_str[ind]
+                new_c = y_conf[ind]
+                ind += 1
+
+            if not new_text or new_t.upper() in self.dict_cap_words or new_t[:-1].upper() in self.dict_cap_words:
+                new_t = new_t[0].upper() + new_t[1:]
+                new_c = 1.
+
+            if join_word:
+                join_word = False
+                new_text[-1] += new_t
+
+            elif new_t == '-':
+                join_word = True
+                new_text[-1] += new_t
+
+            else:
+                new_text.append(new_t) if new_t != '¡' else new_text.append('<UNK>')
+                new_confidence.append(new_c)
+
+        return new_text, new_confidence
+
+    def inference(self, data_loader: torch.utils.data.DataLoader, tokenizer_inference):
+        """
+        Pass to the model the row transcription and get the capitalized and punctuated transcription inferred.
+
+        Parameters
+        ----------
+        data_loader: DataLoader from torch
+            Pytorch Objet with the raw transcription without capitalization or punctuation.
+
+        tokenizer_inference: Tokenizer
+            Transformer Object that transform words into tokens embeddings and vice versa.
+
+        Returns
+        ----------
+        y_str: List with all the sub-words and tokens predicted by the model.
+        y_conf: List with the prediction confidence of the model for each element in y_str.
+       """
+        y_str = []
+        y_conf = []
+        num_iteration = 0
+
+        deep_punctuation = self.model.eval()
+        with torch.no_grad():
+            for x, y, att, y_mask in tqdm(data_loader, desc='test'):
+
+                x, y, att, y_mask = x.to(self.device), y.to(self.device), att.to(self.device), y_mask.to(self.device)
+                y_predict = deep_punctuation(x, att)
+                logits = torch.nn.functional.softmax(y_predict, dim=1)
+                y = y.view(-1)
+                y_predict = y_predict.view(-1, y_predict.shape[2])
+                y_predict = torch.argmax(y_predict, dim=1).view(-1)
+
+                batch_conf = []
+                for b in range(logits.size()[0]):
+                    for s in range(logits.size()[1]):
+                        batch_conf.append(torch.max(logits[b, s, :]).item())
+
+                num_iteration += 1
+                x_tokens = tokenizer_inference.convert_ids_to_tokens(x.view(-1))
+                for index in range(y.shape[0]):
+                    x_tokens[index] = transformation_dict[y_predict[index].item()](x_tokens[index])
+                y_str.append(x_tokens)
+                y_conf.append(batch_conf)
+
+        y_str = list(itertools.chain.from_iterable(y_str))
+        y_conf = list(itertools.chain.from_iterable(y_conf))
+
+        return y_str, y_conf
+
+    def apply(self, input_text: list):
+        """
+        Apply the ACRS to an input raw transcription.
+
+        Parameters
+        ----------
+        input_text: list
+            List of each word of the raw transcription.
+
+        Returns
+        ----------
+        text: List with all the sub-words and tokens predicted by the model.
+        confidence: List with the prediction confidence of the model for each element in y_str.
+        """
+
+        tokenizer_inference = ACPRS_MODELS[self.model_id][2].from_pretrained(self.model_path)
+        token_style = ACPRS_MODELS[self.model_id][4]
+
+        test_set = [Dataset(input_text, tokenizer_c=tokenizer_inference, sequence_len=self.seq_length,
+                            token_style=token_style, is_train=False)]
+
+        # Data Loaders
+        data_loader_params = {'batch_size': self.batch_sz, 'shuffle': False, 'num_workers': 0}
+        test_loaders = [torch.utils.data.DataLoader(utt, **data_loader_params) for utt in test_set]
+
+        y_str, y_conf = self.inference(test_loaders[0], tokenizer_inference)
+        text, confidence = self.get_final_transcription_and_confidence(y_conf, y_str)
+
+        return text, confidence
 
 
 class Pipeline:
@@ -331,17 +604,47 @@ class Pipeline:
         self.name = arguments.name
         self.id = uuid.uuid4()
 
-        self.audio = Audio(self.arguments.data_path)
-        self.trans = RichTranscription(self.arguments.data_path)
+        self.alr = ALR(self.arguments.alr_model)
         self.vad = VAD(self.arguments.vad_model)
-        self.diariz = Diarization(self.arguments.diarization_model)
+        self.drz = Diarization(self.arguments.diarization_model, self.arguments.drz_thresholds)
+
+        self.language = self.alr.identify_language() if self.arguments.language == 'UNK' else arguments.language
+        self.asr = ASR(f"{self.arguments.asr_model}-{self.language}")
+        self.acpr = ACPRS(f"{self.arguments.asr_model}-{self.language}")
+
+        self.multimedia = Multimedia(self.arguments.data_path, self.language)
+        self.trans: Transcription = None
 
     def apply_vad(self):
-        speech_segments, acoustic_segments = self.vad.apply(self.audio)
-        self.audio.set_speech(speech_segments)
-        self.audio.set_acoustic(acoustic_segments)
+        speech_segments, acoustic_segments = self.vad.apply(self.multimedia)
+        self.multimedia.set_speech(speech_segments)
+        self.multimedia.set_acoustic(acoustic_segments)
         return speech_segments, acoustic_segments
 
     def apply_diarization(self):
-        pass
+        print('Diarization!')
+        spk_drz = self.drz.apply(self.multimedia)
+        self.multimedia.annotations = spk_drz
+        return spk_drz
+
+    def apply_asr(self):
+        print('ASR')
+        self.trans = self.asr.apply(self.multimedia)
+
+    def apply_acpr(self):
+        new_transcription, acpr_confidences = self.acpr.apply(self.trans.get_text().split())
+        self.trans.update_transcription(words=new_transcription, acpr_confs=acpr_confidences)
+
+    def make_srt(self):
+        self.trans.update_spk_from_annotations(self.multimedia.annotations)
+        subtitle = Subtitle(self.trans.get_annotation())
+        subtitle.write_srt(self.multimedia.path)
+
+    def apply_drz_and_asr(self):
+        process_1 = multiprocessing.Process(name='diar', target=self.apply_diarization)
+        process_2 = multiprocessing.Process(name='asr', target=self.apply_asr)
+        process_1.start()
+        process_2.start()
+        while process_1.is_alive() or process_2.is_alive():
+            pass
 
